1bb31d3a6fdfbb92ea3bb8b4a344bae29f87efac
Anastasia Ter <nastasia_ie@ukr.net>
2016-01-10 18:41:53 +0100

    DoubleEndedLinkedList

--- src/main/java/DoubleEndedLinkedList.java
+++ src/main/java/DoubleEndedLinkedList.java
@@ -1,5 +1,60 @@
 
 public class DoubleEndedLinkedList {
+    Neighbor firstLink;
+    Neighbor lastLink;
+
+    public void insertInFirstPosition(String houseOwnerName, int houseNumber){
+        Neighbor newLink = new Neighbor(houseOwnerName, houseNumber);
+
+        if(isEmpty()) lastLink = newLink;
+
+        firstLink = newLink.next;
+        lastLink = newLink;
+    }
+
+    public void insertInLastPosition(String houseOwnerName, int houseNumber){
+        Neighbor newLink = new Neighbor(houseOwnerName, houseNumber);
+
+        if(isEmpty()){
+            firstLink = newLink;
+        } else {
+            lastLink.next = newLink;
+        }
+        lastLink = newLink;
+    }
+
+    public void insertAfterKey(String houseOwnerName, int houseNumber, int key){
+
+    }
+
+    public void display(){
+         Neighbor tempLink = firstLink;
+
+        if(tempLink == null) {
+            System.out.println("Sorry, empty DoubleLinkedList");
+        }
+        while (tempLink != null) {
+            tempLink.display();
+            System.out.println("The next Link: " + tempLink.next);
+            tempLink = tempLink.next;
+            System.out.println();
+        }
+    }
+
+    public boolean isEmpty(){
+        return firstLink == null;
+    }
+
+    public static void main(String[] args) {
+        DoubleEndedLinkedList linkedList = new DoubleEndedLinkedList();
+
+        linkedList.insertInFirstPosition("Harry", 5);
+        linkedList.insertInFirstPosition("Ron", 8);
+        linkedList.insertInLastPosition("Hermione", 1);
+        linkedList.insertInFirstPosition("Ron", 10 );
+
+        linkedList.display();
+    }
 
 }
 

1286b494353269183ac0d0df3cd60ba27fcde783
Anastasia Ter <nastasia_ie@ukr.net>
2016-02-03 22:36:33 +0100

    added a few methods

--- src/main/java/BigONotation.java
+++ src/main/java/BigONotation.java
@@ -31,11 +31,11 @@ public class BigONotation {
         return index;
     }
 
-    private void bubbleSort(int[] array) { // O(n2) in worth case
-        for (int i = array.length - 1; i >  1; i--) {
+    private void bubbleSort() { // O(n2) in worth case
+        for (int i = array.length - 1; i > 1; i--) {
             for (int j = 0; j < i; j++) {
                 if(array[j] > array[j+1])
-                    swap (j, j+1);
+                    swap(j, j+1);
             }
         }
     }
@@ -46,26 +46,56 @@ public class BigONotation {
         array[indexTwo] = temp;
     }
 
-    public static void main(String[] args) {
-        BigONotation testArray1 = new BigONotation(1000000);
-        testArray1.generateRandomNumbers();
-        testArray1.addIntToEndOfArray(1);
+    private int binarySearch(int value) { // O(log n)
+        int highIndex = array.length - 1;
+        int lowIndex = 0;
+        int index = -1;
 
-        testArray1.startTime = System.currentTimeMillis();
-        testArray1.linearSearchForValue(1);
-        testArray1.endTime = System.currentTimeMillis();
-
-        System.out.println("Time in ms in case of 1m values: " + (testArray1.endTime - testArray1.startTime));
-
-        BigONotation testArray2 = new BigONotation(100000000);
-        testArray2.generateRandomNumbers();
-        testArray2.addIntToEndOfArray(1);
-
-        testArray2.startTime = System.currentTimeMillis();
-        testArray2.linearSearchForValue(1);
-        testArray2.endTime = System.currentTimeMillis();
-
-        System.out.println("Time in ms in case of 100m values: " + (testArray2.endTime - testArray2.startTime));
+        while (highIndex >= lowIndex) {
+            int middleIndex = (lowIndex + highIndex) / 2;
+            if (value > array[middleIndex]) {
+                lowIndex = middleIndex + 1;
+            } else if (value < array[middleIndex]) {
+                highIndex = middleIndex  - 1;
+            } else {
+                index = middleIndex;
+                lowIndex = highIndex + 1;
+            }
+        }
+        return index;
     }
 
+    public static void main(String[] args) {
+        BigONotation testArray1 = new BigONotation(10000);
+        testArray1.generateRandomNumbers();
+        testArray1.addIntToEndOfArray(1);
+//
+//        testArray1.startTime = System.currentTimeMillis();
+//        testArray1.linearSearchForValue(1);
+//        testArray1.endTime = System.currentTimeMillis();
+//
+//        System.out.println("Time in ms in case of 1m values: " + (testArray1.endTime - testArray1.startTime));
+
+//        BigONotation testArray2 = new BigONotation(100000000);
+//        testArray2.generateRandomNumbers();
+//        testArray2.addIntToEndOfArray(1);
+//
+//        testArray2.startTime = System.currentTimeMillis();
+//        testArray2.linearSearchForValue(1);
+//        testArray2.endTime = System.currentTimeMillis();
+//
+//        System.out.println("Time in ms in case of 100m values: " + (testArray2.endTime - testArray2.startTime));
+
+        testArray1.startTime = System.currentTimeMillis();
+        testArray1.bubbleSort();
+        testArray1.endTime = System.currentTimeMillis();
+
+        System.out.println("Bubble Sort: " + (testArray1.endTime - testArray1.startTime) + " millis");
+
+        testArray1.startTime = System.currentTimeMillis();
+        testArray1.binarySearch(1);
+        testArray1.endTime = System.currentTimeMillis();
+
+        System.out.println("Bubble Sort: " + (testArray1.endTime - testArray1.startTime) + " millis");
+    }
 }
